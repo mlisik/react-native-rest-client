c66461864e02f4483841f85a7e96922370356ea5
Marek Lisik <marek.lisik@holdapp.pl>
2017-11-29 22:20:40 +0100

    transforms empty response (json body length error) into []

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 export default class RestClient {
-  constructor (baseUrl = '', { headers = {}, devMode = false, simulatedDelay = 0 } = {}) {
-    if (!baseUrl) throw new Error('missing baseUrl');
+  constructor(
+    baseUrl = "",
+    { headers = {}, devMode = false, simulatedDelay = 0 } = {}
+  ) {
+    if (!baseUrl) throw new Error("missing baseUrl");
     this.headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     };
     Object.assign(this.headers, headers);
     this.baseUrl = baseUrl;
@@ -11,7 +14,7 @@ export default class RestClient {
     this.devMode = devMode;
   }
 
-  _simulateDelay () {
+  _simulateDelay() {
     return new Promise(resolve => {
       setTimeout(() => {
         resolve();
@@ -19,15 +22,16 @@ export default class RestClient {
     });
   }
 
-  _fullRoute (url) {
+  _fullRoute(url) {
     return `${this.baseUrl}${url}`;
   }
 
-  _fetch (route, method, body, isQuery = false) {
-    if (!route) throw new Error('Route is undefined');
+  _fetch(route, method, body, isQuery = false) {
+    console.log("REQUEST: ", route, method, body, isQuery);
+    if (!route) throw new Error("Route is undefined");
     var fullRoute = this._fullRoute(route);
     if (isQuery && body) {
-      var qs = require('qs');
+      var qs = require("qs");
       const query = qs.stringify(body);
       fullRoute = `${fullRoute}?${query}`;
       body = undefined;
@@ -40,19 +44,29 @@ export default class RestClient {
       Object.assign(opts, { body: JSON.stringify(body) });
     }
     const fetchPromise = () => fetch(fullRoute, opts);
+    const parse = (response, fallback = []) => {
+      return response.text().then(text => (text ? JSON.parse(text) : fallback));
+    };
     if (this.devMode && this.simulatedDelay > 0) {
       // Simulate an n-second delay in every request
       return this._simulateDelay()
         .then(() => fetchPromise())
-        .then(response => response.json());
+        .then(parse);
     } else {
-      return fetchPromise()
-        .then(response => response.json());
+      return fetchPromise().then(parse);
     }
   }
 
-  GET (route, query) { return this._fetch(route, 'GET', query, true); }
-  POST (route, body) { return this._fetch(route, 'POST', body); }
-  PUT (route, body) { return this._fetch(route, 'PUT', body); }
-  DELETE (route, query) { return this._fetch(route, 'DELETE', query, true); }
+  GET(route, query) {
+    return this._fetch(route, "GET", query, true);
+  }
+  POST(route, body) {
+    return this._fetch(route, "POST", body);
+  }
+  PUT(route, body) {
+    return this._fetch(route, "PUT", body);
+  }
+  DELETE(route, query) {
+    return this._fetch(route, "DELETE", query, true);
+  }
 }
